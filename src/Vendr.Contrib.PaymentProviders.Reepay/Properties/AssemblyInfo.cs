523e49208e80edfc5c2558c7444c4f4fb6f9f67d
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-04-03 09:51:24 +0200

    More renaming

--- src/Vendr.Contrib.PaymentProviders.Reepay/Properties/AssemblyInfo.cs
+++ src/Vendr.Contrib.PaymentProviders.Reepay/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vendr.Contrib.PaymentProviders.Template")]
+[assembly: AssemblyTitle("Vendr.Contrib.PaymentProviders.Reepay")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vendr.Contrib.PaymentProviders.Template")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Vendr.Contrib.PaymentProviders.Reepay")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
