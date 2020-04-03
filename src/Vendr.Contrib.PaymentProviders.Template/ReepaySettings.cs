c047e7a26fccbe80f669418f5730046908073794
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-04-03 09:19:51 +0200

    Add more settings.

--- src/Vendr.Contrib.PaymentProviders.Template/ReepaySettings.cs
+++ src/Vendr.Contrib.PaymentProviders.Template/ReepaySettings.cs
@@ -4,9 +4,34 @@ namespace Vendr.Contrib.PaymentProviders
 {
     public class ReepaySettings
     {
-        [PaymentProviderSetting(Name = "Continue URL", Description = "The URL to continue to after this provider has done processing. eg: /continue/")]
+        [PaymentProviderSetting(Name = "Continue URL",
+            Description = "The URL to continue to after this provider has done processing. eg: /continue/",
+            SortOrder = 100)]
         public string ContinueUrl { get; set; }
 
+        [PaymentProviderSetting(Name = "Cancel URL",
+            Description = "The URL to return to if the payment attempt is canceled. eg: /cancel/",
+            SortOrder = 200)]
+        public string CancelUrl { get; set; }
+
+        [PaymentProviderSetting(Name = "Error URL",
+            Description = "The URL to return to if the payment attempt errors. eg: /error/",
+            SortOrder = 300)]
+        public string ErrorUrl { get; set; }
+
+        [PaymentProviderSetting(Name = "Language",
+            Description = "The language of the payment portal to display.",
+            SortOrder = 900)]
+        public string Lang { get; set; }
+
+        [PaymentProviderSetting(Name = "Accepted Payment Methods",
+            Description = "A comma separated list of Payment Methods to accept.",
+            SortOrder = 1000)]
+        public string PaymentMethods { get; set; }
+
+        [PaymentProviderSetting(Name = "Test Mode",
+            Description = "Set whether to process payments in test mode.",
+            SortOrder = 10000)]
         public bool TestMode { get; set; }
     }
 }
