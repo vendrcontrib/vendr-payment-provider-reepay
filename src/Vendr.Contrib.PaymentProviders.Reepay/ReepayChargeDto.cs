1882c5a4cad2224dfcb3de4b03d49f268a0732d7
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-04-05 18:10:11 +0200

    Add methods to handle payments, e.g. cancel, capture and refund.

---
+++ src/Vendr.Contrib.PaymentProviders.Reepay/ReepayChargeDto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Vendr.Contrib.PaymentProviders.Reepay
+{
+    /// <summary>
+    /// Reepay charge object: https://reference.reepay.com/api/#charge
+    /// </summary>
+    [DataContract]
+    public class ReepayChargeDto
+    {
+        [DataMember(Name = "handle")]
+        public string Handle { get; set; }
+
+        [DataMember(Name = "state")]
+        public string State { get; set; }
+
+        [DataMember(Name = "customer")]
+        public string Customer { get; set; }
+
+        [DataMember(Name = "amount")]
+        public int Amount { get; set; }
+
+        [DataMember(Name = "currency")]
+        public string Currency { get; set; }
+
+        [DataMember(Name = "authorized")]
+        public DateTime? Authorized { get; set; }
+
+        [DataMember(Name = "settled")]
+        public DateTime? Settled { get; set; }
+
+        [DataMember(Name = "cancelled")]
+        public DateTime? Cancelled { get; set; }
+
+        [DataMember(Name = "created")]
+        public DateTime Created { get; set; }
+
+        [DataMember(Name = "transaction")]
+        public string Transaction { get; set; }
+
+        [DataMember(Name = "error")]
+        public string Error { get; set; }
+
+        [DataMember(Name = "error_state")]
+        public string ErrorState { get; set; } 
+
+        [DataMember(Name = "processing")]
+        public bool Processing { get; set; }
+
+        [DataMember(Name = "refunded_amount")]
+        public int RefundedAmount { get; set; }
+
+        [DataMember(Name = "authorized_amount")]
+        public int AuthorizedAmount { get; set; }
+
+    }
+}
--- src/Vendr.Contrib.PaymentProviders.Reepay/ReepayPaymentProvider.cs
+++ src/Vendr.Contrib.PaymentProviders.Reepay/ReepayPaymentProvider.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vendr.Contrib.PaymentProviders.Reepay;
 using Vendr.Core;
 using Vendr.Core.Models;
 using Vendr.Core.Web.Api;
@@ -128,16 +129,178 @@ namespace Vendr.Contrib.PaymentProviders
 
         public override CallbackResult ProcessCallback(OrderReadOnly order, HttpRequestBase request, ReepaySettings settings)
         {
-            return new CallbackResult
+            try
             {
-                TransactionInfo = new TransactionInfo
+                // Process callback
+
+
+                return new CallbackResult
                 {
-                    AmountAuthorized = order.TotalPrice.Value.WithTax,
-                    TransactionFee = 0m,
-                    TransactionId = Guid.NewGuid().ToString("N"),
-                    PaymentStatus = PaymentStatus.Authorized
-                }
-            };
+                    TransactionInfo = new TransactionInfo
+                    {
+                        AmountAuthorized = order.TotalPrice.Value.WithTax,
+                        TransactionFee = 0m,
+                        TransactionId = Guid.NewGuid().ToString("N"),
+                        PaymentStatus = PaymentStatus.Authorized
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                Vendr.Log.Error<ReepayPaymentProvider>(ex, "Reepay - ProcessCallback");
+            }
+
+            return CallbackResult.Empty;
+        }
+
+        public override ApiResult FetchPaymentStatus(OrderReadOnly order, ReepaySettings settings)
+        {
+            // Get charge: https://reference.reepay.com/api/#get-charge
+
+            try
+            {
+                var basicAuth = Base64Encode(settings.PrivateKey + ":");
+                var handle = order.OrderNumber;
+
+                var payment = $"https://api.reepay.com/v1/charge/{handle}"
+                            .WithHeader("Authorization", "Basic " + basicAuth)
+                            .WithHeader("Content-Type", "application/json")
+                            .GetJsonAsync<ReepayChargeDto>().Result;
+
+                return new ApiResult()
+                {
+                    TransactionInfo = new TransactionInfoUpdate()
+                    {
+                        TransactionId = GetTransactionId(payment),
+                        PaymentStatus = GetPaymentStatus(payment)
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                Vendr.Log.Error<ReepayPaymentProvider>(ex, "Reepay - FetchPaymentStatus");
+            }
+
+            return ApiResult.Empty;
+        }
+
+        public override ApiResult CancelPayment(OrderReadOnly order, ReepaySettings settings)
+        {
+            // Cancel charge: https://reference.reepay.com/api/#cancel-charge
+
+            try
+            {
+                var basicAuth = Base64Encode(settings.PrivateKey + ":");
+                var handle = order.OrderNumber;
+
+                var payment = $"https://api.reepay.com/v1/charge/{handle}/cancel"
+                            .WithHeader("Authorization", "Basic " + basicAuth)
+                            .WithHeader("Content-Type", "application/json")
+                            .PostAsync(null)
+                            .ReceiveJson<ReepayChargeDto>().Result;
+
+                return new ApiResult()
+                {
+                    TransactionInfo = new TransactionInfoUpdate()
+                    {
+                        TransactionId = order.TransactionInfo.TransactionId,
+                        PaymentStatus = GetPaymentStatus(payment)
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                Vendr.Log.Error<ReepayPaymentProvider>(ex, "Reepay - CancelPayment");
+            }
+
+            return ApiResult.Empty;
+        }
+
+        public override ApiResult CapturePayment(OrderReadOnly order, ReepaySettings settings)
+        {
+            // Create charge: https://reference.reepay.com/api/#create-charge
+
+            try
+            {
+                var basicAuth = Base64Encode(settings.PrivateKey + ":");
+                var handle = order.OrderNumber;
+
+                var payment = $"https://api.reepay.com/v1/charge"
+                    .WithHeader("Authorization", "Basic " + basicAuth)
+                    .WithHeader("Content-Type", "application/json")
+                    .PostJsonAsync(new
+                    {
+                        handle = handle,
+                        amount = AmountToMinorUnits(order.TransactionInfo.AmountAuthorized.Value),
+                        settle = true
+                    })
+                    .ReceiveJson<ReepayChargeDto>().Result;
+            }
+            catch (Exception ex)
+            {
+                Vendr.Log.Error<ReepayPaymentProvider>(ex, "Reepay - CapturePayment");
+            }
+
+            return ApiResult.Empty;
+        }
+
+        public override ApiResult RefundPayment(OrderReadOnly order, ReepaySettings settings)
+        {
+            // Create refund: https://reference.reepay.com/api/#create-refund
+
+            try
+            {
+                var basicAuth = Base64Encode(settings.PrivateKey + ":");
+                var handle = order.OrderNumber;
+
+                var payment = $"https://api.reepay.com/v1/refund"
+                    .WithHeader("Authorization", "Basic " + basicAuth)
+                    .WithHeader("Content-Type", "application/json")
+                    .PostJsonAsync(new
+                    {
+                        invoice = handle,
+                        amount = AmountToMinorUnits(order.TransactionInfo.AmountAuthorized.Value)
+                    })
+                    .ReceiveJson<ReepayChargeDto>().Result;
+            }
+            catch (Exception ex)
+            {
+                Vendr.Log.Error<ReepayPaymentProvider>(ex, "Reepay - RefundPayment");
+            }
+
+            return ApiResult.Empty;
+        }
+
+        protected PaymentStatus GetPaymentStatus(ReepayChargeDto payment)
+        {
+            // Possible Payment statuses:
+            // - authorized
+            // - settled
+            // - failed
+            // - cancelled
+            // - pending
+
+            if (payment.State == "authorized")
+                return PaymentStatus.Authorized;
+
+            if (payment.State == "settled")
+                return PaymentStatus.Captured;
+
+            if (payment.State == "failed")
+                return PaymentStatus.Error;
+
+            if (payment.State == "cancelled")
+                return PaymentStatus.Cancelled;
+
+            if (payment.State == "pending")
+                return PaymentStatus.PendingExternalSystem;
+
+            return PaymentStatus.Initialized;
+        }
+
+        protected string GetTransactionId(ReepayChargeDto payment)
+        {
+            return payment?.Transaction;
         }
     }
 }
