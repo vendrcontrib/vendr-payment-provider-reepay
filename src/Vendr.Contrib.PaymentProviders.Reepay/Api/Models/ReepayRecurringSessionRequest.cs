266c2ca633660535b433247e3a6445dd84bf3b7b
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-06-21 00:02:13 +0200

    Update recurring session request object

--- src/Vendr.Contrib.PaymentProviders.Reepay/Api/Models/ReepayRecurringSessionRequest.cs
+++ src/Vendr.Contrib.PaymentProviders.Reepay/Api/Models/ReepayRecurringSessionRequest.cs
@@ -4,6 +4,12 @@ namespace Vendr.Contrib.PaymentProviders.Reepay.Api.Models
 {
     public class ReepayRecurringSessionRequest : ReepaySessionRequestBase
     {
+        [JsonProperty("customer")]
+        public string Customer { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
         [JsonProperty("create_customer")]
         public ReepayCustomer CreateCustomer { get; set; }
     }
